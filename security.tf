71c07ac6392342eb3a655984cee29300e5060657
paul <paul.thomas@codeworldwide.com>
2021-10-04 19:28:54 +0100

    separate security group rules

--- security.tf
+++ security.tf
@@ -2,61 +2,70 @@ resource "aws_security_group" "bastion-sg" {
   name   = "bastion-access-security-group"
   vpc_id = data.aws_vpc.bastion-vpc.id
 
-  dynamic "ingress" {
-    for_each = var.bastion_config.ingress_blocks
-    content {
-      protocol    = "tcp"
-      from_port   = 22
-      to_port     = 22
-      cidr_blocks = [ingress.value["cidr"]]
-      description = "SSH - ${ingress.value["name"]}"
-    }
-  }
-
-  dynamic "ingress" {
-    for_each = var.bastion_config.ingress_blocks
-    content {
-      protocol    = "tcp"
-      from_port   = 80
-      to_port     = 80
-      cidr_blocks = [ingress.value["cidr"]]
-      description = "HTTP - ${ingress.value["name"]}"
-    }
-  }
-
-  ingress {
-    protocol    = "tcp"
-    from_port   = 80
-    to_port     = 80
-    self        = true
-    description = "onward HTTP from this group"
-  }
-
-  dynamic "ingress" {
-    for_each = var.bastion_config.ingress_blocks
-    content {
-      protocol    = "tcp"
-      from_port   = 443
-      to_port     = 443
-      cidr_blocks = [ingress.value["cidr"]]
-      description = "HTTPS - ${ingress.value["name"]}"
-    }
-  }
-
-  ingress {
-    protocol    = "tcp"
-    from_port   = 443
-    to_port     = 443
-    self        = true
-    description = "onward HTTPS from this group"
-  }
-
-  egress {
-    protocol    = -1
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = var.bastion_config.egress_blocks
-  }
-
   tags = merge(local.tags, { Name = "bastion-access-security-group" })
+}
+
+resource "aws_security_group_rule" "bastion_ingress_ssh" {
+  for_each = var.bastion_config.ingress_blocks
+
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 22
+  to_port           = 22
+  cidr_blocks       = [each.value["cidr"]]
+  description       = "SSH - ${each.key}"
+  security_group_id = aws_security_group.bastion-sg.id
+}
+
+resource "aws_security_group_rule" "bastion_ingress_http" {
+  for_each = var.bastion_config.ingress_blocks
+
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 80
+  to_port           = 80
+  cidr_blocks       = [each.value["cidr"]]
+  description       = "HTTP - ${each.key}"
+  security_group_id = aws_security_group.bastion-sg.id
+}
+
+resource "aws_security_group_rule" "bastion_onward_http" {
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 80
+  to_port           = 80
+  self              = true
+  description       = "onward HTTP from this group"
+  security_group_id = aws_security_group.bastion-sg.id
+}
+
+resource "aws_security_group_rule" "bastion_ingress_https" {
+  for_each = var.bastion_config.ingress_blocks
+
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+  cidr_blocks       = [each.value["cidr"]]
+  description       = "HTTPS - ${each.key}"
+  security_group_id = aws_security_group.bastion-sg.id
+}
+
+resource "aws_security_group_rule" "bastion_onward_https" {
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+  self              = true
+  description       = "onward HTTPS from this group"
+  security_group_id = aws_security_group.bastion-sg.id
+}
+
+resource "aws_security_group_rule" "bastion_egress" {
+  type              = "egress"
+  protocol          = -1
+  from_port         = 0
+  to_port           = 0
+  cidr_blocks       = var.bastion_config.egress_blocks
+  security_group_id = aws_security_group.bastion-sg.id
 }
